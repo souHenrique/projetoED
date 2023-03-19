a01ff397e821e691aa51a0f630cc2cb0e3d88f59
Luis Henrique <128126715+souHenrique@users.noreply.github.com>
2023-03-19 05:27:20 -0300

    Add files via upload
    Adicionada função para exibir os filmes e estrutura de repetição.

--- Projeto_ED.py
+++ Projeto_ED.py
@@ -9,18 +9,31 @@ class Lista_de_Filmes:
     def __init__(self):
         self.head = None
     def adiciona_filmes(self, nome_filme, ano, genero, diretor):
-        filme_novo = Filme(nome_filme, ano, genero, diretor)
+        filme_novo = Filmes(nome_filme, ano, genero, diretor)
         filme_novo.next = self.head
         self.head = filme_novo
+    def mostrar_filmes(self):
+        filme_atual = self.head
+        while filme_atual is not None:
+            print("Nome do filme:", filme_atual.nome_filme)
+            print("Ano de lançamento:", filme_atual.ano)
+            print("Gênero:", filme_atual.genero)
+            print("Diretor:", filme_atual.diretor)
+            print("=" * 30)
+            filme_atual = filme_atual.next
 lista_Filmes = Lista_de_Filmes()
-print(lista_Filmes)
-
-nome_filme = input("Digite o nome do filme:")
-ano = int(input("Digiter o ano de lançamento do filme:"))
-genero = input("Digite o genero do filme:")
-diretor = input("Digite o nome do dirtetor do filme:")
-
-print("Nome do filme:", nome_filme)
-print("Ano de lançamento:", ano)
-print("Gênero:", genero)
-print("Diretor do filme", diretor)
+while True:
+    print("1- Adicionar Filme")
+    print("2- Mostrar Filmes")
+    print("3- Fechar")
+    opcao = int(input("Digite a opção:"))
+    if opcao == 1:
+        nome_filme = input("Digite o nome do filme:")
+        ano = int(input("Digiter o ano de lançamento do filme:"))
+        genero = input("Digite o gênero do filme:")
+        diretor = input("Digite o nome do diretor do filme:")
+        lista_Filmes.adiciona_filmes(nome_filme, ano, genero, diretor)
+    elif opcao == 2:
+        lista_Filmes.mostrar_filmes() 
+    elif opcao == 3:
+        break
